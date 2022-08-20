059020b4eba858348b5dfa0037067eccbd4e44b5
Aleix <aleix.olle@students.salle.url.edu>
2022-08-20 21:23:13 +0200

    when user sends opinion, we receive an email

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './input.css'
 
-
 createApp(App).mount('#app')
 
 const application = createApp(App)
--- src/pages/Feedback.vue
+++ src/pages/Feedback.vue
@@ -4,6 +4,11 @@
     <div id="feedbackSection" class="grid grid-cols-2 ">
       <section id="opinion" class="flex flex-col">
         <p class="font-roboto text-xl text-center pb-24">Give your opinion about the different icons we have created</p>
+        <form ref="form" @submit.prevent="sendEmail" class="flex flex-col">
+          <textarea v-model="message" type="text" id="message" name="message" class="mx-28 h-32 rounded-3xl shadow-inner font-roboto"></textarea>
+          <input type="submit" value="Send" data-mdb-ripple="true"
+                 data-mdb-ripple-color="light" class="mx-28 mt-28 shadow-md bg-white rounded-3xl font-montserrat  h-16 text-2xl hover:bg-slate-50 active:bg-slate-200">
+        </form>
       </section>
       <section id="design" class="flex flex-col">
         <p class="font-roboto text-xl text-center pb-24">If you think you have a good design, send us your propose and we will analise it to keep improving</p>
@@ -14,31 +19,28 @@
 
 
 <script>
+import emailjs from 'emailjs-com';
 
 export default {
   name: "FeedbackPage",
-  components: {
-
-  },
-  data: function() {
+  data() {
     return {
-      config: {
-        options: [
-          {
-            value: "option 1"
-          },
-          {
-            value: "option 2"
-          },
-          {
-            value: "option 3"
-          },
-        ],
-        prefix: "The",
-        backgroundColor: "green",
-        disabled: false,
-      }
+      message: ''
     }
+  },
+  methods: {
+    sendEmail() {
+      try {
+        console.log(this.message)
+        emailjs.sendForm('service_a4gmiv4', 'template_edecbp1', this.$refs.form,
+            'a4_ijyWw1e5xdKrY3')
+
+      } catch(error) {
+        console.log({error})
+      }
+      // Reset form field
+      this.message = ''
+    },
   }
 }
 </script>
