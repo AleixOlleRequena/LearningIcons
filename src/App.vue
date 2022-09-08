01b66a9ec53928efde7f80e189856f230231a3a9
Aleix <aleix.olle@students.salle.url.edu>
2022-09-08 13:10:24 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,3 @@
-<!--
-LA CLASS DE FOOTER PAGE S'HA DE CANVIAR A CADA PÀGINA DEPENENT DE LES NECESSITATS
-class="absolute -bottom-80 pb-4 pl-2" CLASS DE FOOTER A MAINPAGE
-class="relative self-start -bottom-36 pl-2 scrollbar-hide" CLASS DE FOOTER A LEARNING ICONS
-class="absolute self-start bottom-0 pl-2 scrollbar-hide" CLASS DE FOOTER A INFORM PAGE i FEEDBACK-->
-
 <template>
   <div class="h-auto w-auto flex flex-col mt-5">
     <Header></Header>
