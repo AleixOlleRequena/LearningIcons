23e4f0425169c28d61003a3f058b781211d60df1
Aleix <aleix.olle@students.salle.url.edu>
2022-09-12 13:33:34 +0200

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,10 @@
 <template>
 <section>
   <div class="flex flex-row justify-center mt-8 md:-mt-8">
-    <svg-icon class="cursor-pointer h-8 md:h-32" type="mdi" :path= "twitter" width=55 height=55 color="white" @click="openWeb('https://www.upc.edu/')"></svg-icon>
-    <svg-icon class="cursor-pointer h-8 md:h-32" type="mdi" :path="facebook" width=55 height=55 color="white" @click="openWeb('https://www.upc.edu/')"></svg-icon>
+    <svg-icon class="cursor-pointer h-8 md:h-32" type="mdi" :path= "twitter" width=55 height=55 color="white" @click="openWeb('https://twitter.com/LaSalleBCN?s=20&t=nyXeVw5dKN_hQ86QbN2GSA')"></svg-icon>
+    <svg-icon class="cursor-pointer h-8 md:h-32" type="mdi" :path="facebook" width=55 height=55 color="white" @click="openWeb('https://www.facebook.com/LaSalleBCN')"></svg-icon>
     <svg-icon class="cursor-pointer h-8 md:h-32" type="mdi" :path="blog" width=55 height=55 color="white" @click="openWeb('https://blogs.salleurl.edu/en/technology-learning')"></svg-icon>
+    <svg-icon class="cursor-pointer h-8 md:h-32" type="mdi" :path="github" width=55 height=55 color="white" @click="openWeb('https://github.com/LS-LEDA')"></svg-icon>
   </div>
 </section>
 </template>
@@ -11,8 +12,10 @@
 <script>
 import {
   mdiFacebook,
+  mdiGithub,
   mdiTwitter,
-  mdiWeb
+  mdiWeb,
+
 } from "@mdi/js";
 import SvgIcon from '@jamescoyle/vue-icon'
 
@@ -25,7 +28,8 @@ export default {
     return {
       twitter: mdiTwitter,
       facebook: mdiFacebook,
-      blog: mdiWeb
+      blog: mdiWeb,
+      github: mdiGithub
     }
   },
   methods:{
